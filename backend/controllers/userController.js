2726283da2a3bc513a88b7c2adaea325e37dc471
Sahil0705 <sahildonde7@gmail.com>
2025-01-16 09:12:16 +0530

    added {secure: true} key when server sends cookies to client

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,7 +63,8 @@ export const login = async (req, res) => {
 
         const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' }).json({
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict', secure: process.env.NODE_ENV !== "development",
+        }).json({
             _id: user._id,
             username: user.username,
             fullName: user.fullName,
